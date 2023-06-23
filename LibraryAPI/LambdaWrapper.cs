4b27fbd2b29cf42811a53f34cab97cf0a153bc42
Hunter Dubbs <hdubbs@westfaliausa.com>
2023-06-22 23:39:19 -0400

    upgrade to .net 6 and created aws lambda entry point

---
+++ LibraryAPI/LambdaWrapper.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace LibraryAPI
+{
+    public class LambdaWrapper : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>()
+                .UseLambdaServer();
+        }
+    }
+}
